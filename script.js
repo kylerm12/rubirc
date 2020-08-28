80a3343d9a9f40489eb20f312b87191056a65155
kyler <kylermartinez11@gmail.com>
2020-08-28 14:50:58 -0500

    new

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var x;	
+x= prompt("Hey what's your name?");
+	alert("Hey "+x+" Nice to meet you!");
+
+var a = 5;
+var b = 5;
+var theNumbersMatch
+
+if ( a == b ) {
+    theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+console.log(theNumbersMatch);
