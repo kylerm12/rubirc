6c089477873505cf933b394453e8e9f0bfcb4fb4
kyler <kylermartinez11@gmail.com>
2020-08-28 15:26:29 -0500

    inner

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var x;	
 x= prompt("Hey what's your name?");
 	alert("Hey "+x+" Nice to meet you!");
-
+//
 var a = 5;
 var b = 5;
 var theNumbersMatch
@@ -14,3 +14,9 @@ if ( a == b ) {
 }
 
 console.log(theNumbersMatch);
+//
+var t, r, l;
+t = 5;
+r = 6;
+l = t + r;
+document.getElementById("demo1").innerHTML = l;
